db364a0af8665a196e74bd67ba0b99b8df197a1b
tasi1sean <tasi1sean@gmail.com>
2026-07-09 13:34:47 +0800

    Update zerojudge-j123.cpp

--- zerojudge-j123.cpp
+++ zerojudge-j123.cpp
@@ -2,44 +2,78 @@
 using namespace std;
 int main()
 {
-    int x=int(sqrt(2147483647))+1;
-    vector <bool> vt(x,true);
-    vector <int> v1;
-    vt[0]=false;
-    vt[1]=false;
-    for (int i=0;i<x;i++){
-        if (vt[i]!=false){
-            for (long long j=1LL*i*i;j<x;j=j+i){
-                vt[j]=false;
-            }            
+    //substr(開頭,長度)
+    int n,m;
+    cin >> n >> m;
+    vector<vector<int>> vt(n,vector<int>(m,0));
+    for (int i=0;i<n;i++){
+        for (int j=0;j<m;j++){
+            cin >> vt[i][j];
         }
     }
-    for (int i=0;i<x;i++){
-        if (vt[i]){
-            v1.push_back(i);
+    int ans=0;
+    int oldans=1;
+    while(ans!=oldans){
+        oldans=ans;
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                if (vt[i][j]>0){
+                    for (int k=0;k<n;k++){
+                        if (vt[k][j]==vt[i][j]&&k!=i){
+                            int g=0;
+                            if(k<i){
+                                for (int o=k;o<=i;o++){
+                                    g=vt[o][j]+g;
+                                }
+                                if(g/2==vt[i][j]){
+                                    ans=ans+vt[i][j];
+                                    vt[i][j]=0;
+                                    vt[k][j]=0;
+                                }
+                            }
+                            if(k>i){
+                                for (int o=i;o<=k;o++){
+                                    g=vt[o][j]+g;
+                                }
+                                if(g/2==vt[i][j]){
+                                    ans=ans+vt[i][j];
+                                    vt[i][j]=0;
+                                    vt[k][j]=0;
+                                }
+                            }
+                            
+                        }
+                    }
+                    for (int k=0;k<m;k++){
+                        if (vt[i][k]==vt[i][j]&&k!=j){
+                            int g=0;
+                            if(k<j){
+                                for (int o=k;o<=j;o++){
+                                    g=vt[i][o]+g;
+                                }
+                                if(g/2==vt[i][j]){
+                                    ans=ans+vt[i][j];
+                                    vt[i][j]=0;
+                                    vt[i][k]=0;
+                                }
+                            }
+                            if(k>j){
+                                for (int o=j;o<=k;o++){
+                                    g=vt[i][o]+g;
+                                }
+                                if(g/2==vt[i][j]){
+                                    ans=ans+vt[i][j];
+                                    vt[i][j]=0;
+                                    vt[i][k]=0;
+                                }
+                            }
+                            
+                        }
+                    }
+                }
+            }
         }
     }
-    long long a;
-    while(cin >> a){
-        for (int i=0;i<v1.size();i++){
-            if (a==0||a==1) {
-                cout << "非質數" << "\n";
-                break;
-            }
-            if (a==v1[i]){
-                cout << "質數" << "\n";
-                break;
-            }
-            if (a%v1[i]==0){
-                cout << "非質數" << "\n";
-                break;
-            }
-            if (1LL*(v1[i]) * v1[i] > a&&a%v1[i]!=0){
-                cout << "質數" << "\n";
-                break; 
-            }
-            if (1LL*(v1[i]) * v1[i] > a) break;
-        }
-    }
+    cout << ans << endl;
     return 0;
 }
