4757b51422c5683ac43525bcfd45bfafc7d3e19b
tasi1sean <tasi1sean@gmail.com>
2026-07-11 14:44:39 +0800

    Update zerojudge-n912.cpp

--- zerojudge-n912.cpp
+++ zerojudge-n912.cpp
@@ -1,79 +1,55 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    //substr(開頭,長度)
-    int n,m;
-    cin >> n >> m;
-    vector<vector<int>> vt(n,vector<int>(m,0));
-    for (int i=0;i<n;i++){
-        for (int j=0;j<m;j++){
-            cin >> vt[i][j];
-        }
-    }
-    int ans=0;
-    int oldans=1;
-    while(ans!=oldans){
-        oldans=ans;
-        for (int i=0;i<n;i++){
-            for (int j=0;j<m;j++){
-                if (vt[i][j]>0){
-                    for (int k=0;k<n;k++){
-                        if (vt[k][j]==vt[i][j]&&k!=i){
-                            int g=0;
-                            if(k<i){
-                                for (int o=k;o<=i;o++){
-                                    g=vt[o][j]+g;
-                                }
-                                if(g/2==vt[i][j]){
-                                    ans=ans+vt[i][j];
-                                    vt[i][j]=0;
-                                    vt[k][j]=0;
-                                }
-                            }
-                            if(k>i){
-                                for (int o=i;o<=k;o++){
-                                    g=vt[o][j]+g;
-                                }
-                                if(g/2==vt[i][j]){
-                                    ans=ans+vt[i][j];
-                                    vt[i][j]=0;
-                                    vt[k][j]=0;
-                                }
-                            }
-                            
-                        }
-                    }
-                    for (int k=0;k<m;k++){
-                        if (vt[i][k]==vt[i][j]&&k!=j){
-                            int g=0;
-                            if(k<j){
-                                for (int o=k;o<=j;o++){
-                                    g=vt[i][o]+g;
-                                }
-                                if(g/2==vt[i][j]){
-                                    ans=ans+vt[i][j];
-                                    vt[i][j]=0;
-                                    vt[i][k]=0;
-                                }
-                            }
-                            if(k>j){
-                                for (int o=j;o<=k;o++){
-                                    g=vt[i][o]+g;
-                                }
-                                if(g/2==vt[i][j]){
-                                    ans=ans+vt[i][j];
-                                    vt[i][j]=0;
-                                    vt[i][k]=0;
-                                }
-                            }
-                            
-                        }
-                    }
-                }
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main() { 
+    int h,w,k; 
+    cin >> h >> w >> k; 
+    vector <vector<int>> vt(h,vector<int>(w,0)); 
+    vector<pair<int,int>> d{{1,0},{0,1},{-1,0},{0,-1}}; 
+    vector<pair<int,int>> d2{{1,1},{1,-1},{-1,1},{-1,-1}}; 
+    
+    for (int i=0;i<h;i++){ 
+        for (int j=0;j<w;j++){ 
+            cin >> vt[i][j]; 
+        } 
+    } 
+    
+    while (k--){ 
+        vector<pair<int,int>> v1, v2;
+        for (int i=0;i<h;i++){ 
+            for (int j=0;j<w;j++){ 
+                if (vt[i][j]==1) v1.push_back({i,j});
+                if (vt[i][j]==-1) v2.push_back({i,j});
             }
         }
-    }
-    cout << ans << endl;
-    return 0;
+        
+        for (auto p : v1){ 
+            for (int i=0;i<4;i++){ 
+                int nh = p.first + d[i].first; 
+                int nw = p.second + d[i].second; 
+                if(nh >= 0 && nh < h && nw >= 0 && nw < w){ 
+                    vt[nh][nw] = 1; 
+                } 
+            } 
+        } 
+        
+        for (auto p : v2){ 
+            if (vt[p.first][p.second] == 1) continue;
+            for (int i=0;i<4;i++){ 
+                int nh = p.first + d2[i].first; 
+                int nw = p.second + d2[i].second; 
+                if(nh >= 0 && nh < h && nw >= 0 && nw < w){ 
+                    if(vt[nh][nw] == 0) vt[nh][nw] = -1; 
+                } 
+            } 
+        } 
+    } 
+    
+    for (int i=0;i<h;i++){ 
+        for (int j=0;j<w;j++){ 
+            cout << vt[i][j] << " "; 
+        } 
+        cout << endl; 
+    } 
+    return 0; 
 }
