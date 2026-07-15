4ce8c482385ebedf5ceed18cbbc5468c534bb361
tasi1sean <tasi1sean@gmail.com>
2026-07-15 10:33:29 +0800

    Update zerojudge-f815.cpp

--- zerojudge-f815.cpp
+++ zerojudge-f815.cpp
@@ -1,56 +1,37 @@
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
-int main() { 
-    int h, w, k; 
-    cin >> h >> w >> k; 
-    
-    vector<vector<int>> vt(h, vector<int>(w, 0)); 
-    vector<vector<int>> v2(h, vector<int>(w, 0)); 
-    
-    vector<pair<int, int>> d{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}; 
-    long long sum = 0, nsum = 0;
-    
-    for (int i = 0; i < h; i++) { 
-        for (int j = 0; j < w; j++) { 
-            cin >> vt[i][j]; 
-            sum += vt[i][j]; 
-        } 
-    } 
-    
-    while (k--) { 
-        for (int j = 0; j < h; j++) { 
-            for (int g = 0; g < w; g++) { 
-                int up = 0, low = 0; 
-                int all=0;
-                
-                for (int i = 0; i < 4; i++) { 
-                    int nh = j + d[i].first; 
-                    int nw = g + d[i].second; 
-                    
-                    
-                    if (nh >= 0 && nh < h && nw >= 0 && nw < w) { 
-                        all++;
-                        if (vt[nh][nw] > vt[j][g]) up++; 
-                        else if (vt[nh][nw] < vt[j][g]) low++; 
-                    } 
-                } 
-                
-               
-                if (up > all/2) v2[j][g] = vt[j][g] + 1; 
-                else if (low > all/2) v2[j][g] = vt[j][g] - 1; 
-                else v2[j][g] = vt[j][g]; 
-            } 
-        } 
-        vt = v2; 
-    } 
-    
-    for (int i = 0; i < h; i++) { 
-        for (int j = 0; j < w; j++) { 
-            nsum += vt[i][j]; 
-        } 
-    } 
-    
-    cout << nsum - sum << "\n"; 
-    return 0; 
+typedef long long ll;
+int n;
+ll c;
+vector<ll> a;
+
+bool check(ll level){
+  ll sum_ = 0;
+  for(int i = 0; i < n; ++i){
+    if(a[i] < level){
+      sum_ += (level-a[i]) * (level-a[i]);
+      if(sum_ > c) return false;
+    }
+  }
+  return true; 
+}
+
+int main(){
+  cin >> n >> c;
+  a.assign(n, 0);
+  for(int i = 0; i < n; ++i) cin >> a[i];
+  ll l = 1, r = 20000000, ans = 0;
+  // 對等級做 2 分搜，因為等級具有單調性
+  while(l <= r){
+    ll mid = (l+r) / 2;
+    if(check(mid)){
+      ans = mid;
+      l = mid + 1;
+    }else{
+      r = mid - 1;
+    }
+  }
+  cout << ans << "\n";
+  return 0;
 }
