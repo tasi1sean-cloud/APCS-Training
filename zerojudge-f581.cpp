0172d783ee8f89edb2a1a235a1df0463cd37f44a
tasi1sean <tasi1sean@gmail.com>
2026-07-22 10:36:23 +0800

    Update zerojudge-f581.cpp

--- zerojudge-f581.cpp
+++ zerojudge-f581.cpp
@@ -1,40 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+typedef long long ll;
 int main() {
-    int n, m;
-    while (cin >> n >> m) {
-      vector<long long> arr(n + 1, 0);
-      vector<long long> sum(n + 1, 0);
-    for (int i = 1; i <= n; i++) {
-      cin >> arr[i];
-          sum[i] = sum[i - 1] + arr[i];
+  int n,m;
+  cin >> n >> m;
+  vector <ll> p(n);
+  vector <ll> primex(n);
+  for (int i=0;i<n;i++){
+    cin >> p[i];
+    if (i!=0) primex[i]=primex[i-1]+p[i];
+    else primex[i]=p[i];
   }
-        // 處理每一次的詢問
-        for (int i = 0; i < m; i++) {
-            long long l, r, a, b;
-            cin >> l >> r >> a >> b;
-
-          long long _sum = sum[r] - sum[l - 1];
-            long long t_val = _sum * a;
-          long long ba = a + b;
-            long long le = l;
-          long long ri = r;
-            long long ans = r; // 預設答案
-
-          while (le<=ri) {
-                long long mid = (le + ri) / 2;
-                long long ent_sum = sum[mid] - sum[l-1];
-                if (ent_sum * ba >= t_val) {
-                    ans = mid;         
-                    ri = mid - 1;  
-                } else {
-                    le = mid + 1; 
-                }
-            }
-
-            cout << ans << "\n";
-        }
+  int now=0;
+  while (m--){
+    ll q;
+    cin >> q;
+    ll target;
+    if (now!=0)  target = q+primex[now-1];
+    else  target = q;
+    while(target>primex[n-1]){
+      now=0;
+      target = target - primex[n-1];
     }
-    return 0;
+    now = ((lower_bound(primex.begin(),primex.end(),target) - primex.begin()) + 1)%n;
+  }
+  cout << now << endl;
+  return 0;
 }
