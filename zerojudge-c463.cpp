b4e0a525901067b84b4c57a6e2648ed7f675e9d5
tasi1sean <tasi1sean@gmail.com>
2026-07-25 11:00:29 +0800

    上課 c463 樹狀圖分析

---
+++ zerojudge-c463.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> adj;
+long long ans = 0;
+
+long long dfs(int root){
+  int s = adj[root].size();
+  if(s == 0) return 1;
+  long long height = 0;
+
+  for(int i = 0; i < s; ++i){
+    int neighbor = adj[root][i];
+    height = max(height, dfs(neighbor));
+  }
+  ans += height;
+  return height + 1;
+}
+
+int main(){
+  int n, root = -1;
+  cin >> n;
+  adj.assign(n+1, {});
+  vector<int> parent(n+1, -1);
+  for(int i = 1; i <= n; ++i){
+    int k;
+    cin >> k;
+    while(k--){
+      int node;
+      cin >> node;
+      adj[i].push_back(node);
+      parent[node] = i;
+    }
+  }
+  for(int i = 1; i <= n; ++i){
+    if(parent[i] == -1){
+      root = i;
+      break;
+    }
+  }
+  dfs(root);
+  cout << root << " " << ans << "\n";
+  return 0;
+}
