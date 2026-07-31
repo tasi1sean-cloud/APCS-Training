230a110146e7982070dc32e9f15fad4a795f8f03
tasi1sean <tasi1sean@gmail.com>
2026-07-31 10:19:58 +0800

    完成 f680 色塊數量 bfs解法

--- zerojudge-f680.cpp
+++ zerojudge-f680.cpp
@@ -6,13 +6,20 @@ int n;
 vector<vector<bool>> visited;
 vector<vector<int>> vt;
 int d[4][2]={{1,0},{0,1},{-1,0},{0,-1}};
-void dfs(int x,int y){
+queue<pair<int,int>> q;
+void bfs(int x,int y){
+  q.push({x,y});
   visited[x][y]=true;
-  for(int i = 0;i < 4; i++){
-    int nx=x+d[i][0],ny=y+d[i][1];
-    if(nx>=0&&nx<n&&ny>=0&&ny<n){
-      if(!visited[nx][ny]&&vt[nx][ny]==vt[x][y]){
-        dfs(nx,ny);
+  while (!q.empty()){
+    int x=q.front().first,y=q.front().second;
+    q.pop();
+    visited[x][y]=true;
+    for(int i=0;i<4;i++){
+      int nx=x+d[i][0],ny=y+d[i][1];
+      if (nx>=0&&nx<n&&ny>=0&&ny<n&&vt[nx][ny]==vt[x][y]){
+        if(!visited[nx][ny]){
+          q.push({nx,ny});
+        }
       }
     }
   }
@@ -29,7 +36,7 @@ int main(){
   }for(int i=0;i<n;i++){
     for (int j=0;j<n;j++){
       if(!visited[i][j] && vt[i][j]!=0){
-        dfs(i,j);
+        bfs(i,j);
         ++ans;
       }
     }
