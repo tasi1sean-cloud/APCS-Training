fd8c4b15ba9a6b85b612044b3e9a5222e8c915dc
tasi1sean <tasi1sean@gmail.com>
2026-08-04 13:18:57 +0800

    Update zerojudge-d908.cpp

--- zerojudge-d908.cpp
+++ zerojudge-d908.cpp
@@ -1,48 +1,34 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
 
-typedef long long ll;
-int h,w;
-vector<vector<int>> vt;
-vector<int> ans;
-int num;
-int d[4][2]={{1,0},{0,1},{-1,0},{0,-1}};
-
 int main(){
-  cin >> h >> w;
-  queue<pair<int,int>> q;
-  vt.assign(h,vector<int>(w,-1));
-  for (int i=0;i<h;i++){
-    for(int j=0;j<w;j++){
-      char ch;
-      cin >> ch;
-      if (ch=='-'){
-        vt[i][j]=0;
-        q.push({i,j});
-      }
-    }
-  }
-   
-  cin >> num;
-  ans.assign(num+1,0);
+  int n;
+  cin >> n;
+  int dir[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+  vector<vector<int>> dist(n, vector<int>(n, -1));
+  int cx, cy;
+  cin >> cx >> cy;
+  queue<pair<int, int>> q;
+  dist[cx][cy] = 0;
+  q.push({cx, cy});
   while(!q.empty()){
-    int x=q.front().first,y=q.front().second;
+    int cx = q.front().first, cy = q.front().second;
     q.pop();
-    for(int i=0;i<4;i++){
-      int nx=x+d[i][0],ny=y+d[i][1];
-      
-      if(nx>=0&&nx<h&&ny>=0&&ny<w&&vt[nx][ny]==-1){
-        q.push({nx,ny});
-        vt[nx][ny]=vt[x][y]+1;
-        if (vt[nx][ny]>num) vt[nx][ny]=1; 
+    for(int i = 0; i < 4; ++i){
+      int nx = cx + dir[i][0], ny = cy + dir[i][1];
+      if(nx >= 0 && nx < n && ny >= 0 && ny < n && dist[nx][ny] == -1){
+        dist[nx][ny] = dist[cx][cy] + 1;
+        q.push({nx, ny});
       }
     }
   }
-  for (int i=0;i<h;i++){
-    for(int j=0;j<w;j++){
-      ans[vt[i][j]]++;
+  for(int i = 0; i < n; ++i){
+    for(int j = 0; j < n; ++j){
+      cout << dist[i][j] << " ";
     }
+    cout << "\n";
   }
-  for (int i=1;i<num+1;i++) cout << ans[i] << " ";
   return 0;
 }
