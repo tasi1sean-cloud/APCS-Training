5de2f437cd352d94abacbcda0996532ecf315392
tasi1sean <tasi1sean@gmail.com>
2026-08-05 10:08:19 +0800

    bfs 解法

--- Leetcode 207 Course Schedule.cpp
+++ Leetcode 207 Course Schedule.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> adj;
-    vector<int> visited;
-
-    bool dfs(int node){
-        if(visited[node] == 2) return true;
-        if(visited[node] == 1) return false;
-        visited[node] = 1;
-        for(int i = 0; i < adj[node].size(); ++i){
-            int neighbor = adj[node][i];
-            if(dfs(neighbor) == false) return false;
-        }
-        visited[node] = 2;
-        return true;
-    }
 
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         adj.assign(numCourses, {});
+        vector<int> indegree(numCourses, 0);
         for(int i = 0; i < prerequisites.size(); ++i){
             int c = prerequisites[i][0], p = prerequisites[i][1];
             adj[p].push_back(c);
+            indegree[c]++;
         }
-        visited.assign(numCourses, 0);
+        queue<int> q;
         for(int i = 0; i < numCourses; ++i){
-            if(visited[i] == 0){
-                if(dfs(i) == false) return false;
+            if(indegree[i] == 0) q.push(i);
+        }
+        int visited = 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ++visited;
+            for(int i = 0; i < adj[node].size(); ++i){
+                int neighbor = adj[node][i];
+                indegree[neighbor]--;
+                if(indegree[neighbor] == 0) q.push(neighbor);
             }
         }
-        return true;
+        return visited == numCourses;
     }
 };
