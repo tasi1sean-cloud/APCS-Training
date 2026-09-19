d57497fb9b914e60670840405112126e77f4e863
tasi1sean <tasi1sean@gmail.com>
2026-09-19 15:32:29 +0800

    Update zerojudge-b967.cpp

--- zerojudge-b967.cpp
+++ zerojudge-b967.cpp
@@ -5,17 +5,29 @@ typedef long long ll;
 vector<vector<ll>> adj;
 ll n;
 vector<bool> visited;
+ll ans=0;
 ll dfs(ll root){
     ll s=adj[root].size();
     visited[root]=true;
     if (s==0) return 1;
-    ll hight = 0;
+    ll max_=0,max1=0;
     for (ll i=0;i<s;i++){
         if (!visited[adj[root][i]]) {
-            hight=max(dfs(adj[root][i]),hight);
+            int x=dfs(adj[root][i]);
+            if (x>max_){
+                max1=max_;
+                max_=x;
+                continue;
+            }
+            if (x>max1){
+                max1=x;
+            }
             }
     }
-    return hight+1;
+    if (s>=2&&max_+max1>ans){
+        ans=max_+max1;
+    }
+    return max_+1;
 }
 
 int main()
@@ -23,8 +35,8 @@ int main()
     cin >> n;
     adj.assign(n,{});
     visited.assign(n,true);
-    for (int i=0;i<n-1;i++){
-        int  a,b;
+    for (ll i=0;i<n-1;i++){
+        ll  a,b;
         cin >> a >> b;
         adj[a].push_back(b);
         visited[b]=false;
@@ -32,7 +44,8 @@ int main()
     for (ll i=0;i<n;i++){
         if (visited[i]){
             visited[i]=false;
-            cout << dfs(i);
+            dfs(i);
+            cout << ans;
             break;
         }
     }
